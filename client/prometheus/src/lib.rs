4882334f72dd588fdf685c042075b47237a87966
hskang9 <hskang9@gmail.com>
2019-11-27 18:00:25 +0900

    fix: refactor prometheus-module

--- client/prometheus/src/lib.rs
+++ client/prometheus/src/lib.rs
@@ -16,44 +16,44 @@ pub mod metrics;
 /// Initializes the metrics context, and starts an HTTP server
 /// to serve metrics.
 pub fn init_prometheus(prometheus_addr: SocketAddr) {
-    let addr = prometheus_addr;
-    let server = Server::bind(&addr)
-        .serve(|| {
-            // This is the `Service` that will handle the connection.
-            // `service_fn_ok` is a helper to convert a function that
-            // returns a Response into a `Service`.
-            service_fn_ok(move |req: Request<Body>| {
-                if req.uri().path() == "/metrics" {
-                    let metric_families = prometheus::gather();
-                    let mut buffer = vec![];
-                    let encoder = TextEncoder::new();
-                    encoder.encode(&metric_families, &mut buffer).unwrap();
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .header("Content-Type", encoder.format_type())
-                        .body(Body::from(buffer))
-                        .expect("Sends OK(200) response with one or more data metrics")
-                } else {
-                    Response::builder()
-                        .status(StatusCode::NOT_FOUND)
-                        .body(Body::from("Not found."))
-                        .expect("Sends NOT_FOUND(404) message with no data metric")
-                }
-            })
-        })
-        .map_err(|e| error!("server error: {}", e));
+  let addr = prometheus_addr;
+  let server = Server::bind(&addr)
+    .serve(|| {
+      // This is the `Service` that will handle the connection.
+      // `service_fn_ok` is a helper to convert a function that
+      // returns a Response into a `Service`.
+      service_fn_ok(move |req: Request<Body>| {
+        if req.uri().path() == "/metrics" {
+          let metric_families = prometheus::gather();
+          let mut buffer = vec![];
+          let encoder = TextEncoder::new();
+          encoder.encode(&metric_families, &mut buffer).unwrap();
+          Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", encoder.format_type())
+            .body(Body::from(buffer))
+            .expect("Sends OK(200) response with one or more data metrics")
+        } else {
+          Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("Not found."))
+            .expect("Sends NOT_FOUND(404) message with no data metric")
+        }
+      })
+    })
+    .map_err(|e| error!("server error: {}", e));
 
-    info!("Exporting metrics at http://{}/metrics", addr);
+  info!("Exporting metrics at http://{}/metrics", addr);
 
-    let mut rt = tokio::runtime::Builder::new()
-        .core_threads(1) // one thread is sufficient
-        .build()
-        .expect("Builds one thread of tokio runtime exporter for prometheus");
+  let mut rt = tokio::runtime::Builder::new()
+    .core_threads(1) // one thread is sufficient
+    .build()
+    .expect("Builds one thread of tokio runtime exporter for prometheus");
 
-    std::thread::spawn(move || {
-        rt.spawn(server);
-        rt.shutdown_on_idle().wait().unwrap();
-    });
+  std::thread::spawn(move || {
+    rt.spawn(server);
+    rt.shutdown_on_idle().wait().unwrap();
+  });
 }
 
 #[macro_export]
@@ -76,17 +76,6 @@ macro_rules! prometheus_histogram(
   }
 );
 
-<<<<<<< HEAD
-#[macro_export]
-macro_rules! prometheus_counter(
-  ($($metric:expr => $value:expr),*) => {
-    use $crate::{metrics::*};
-    $(
-        metrics::set_counter(&$metric, $value);
-    )*
-  }
-);
-
 /*
 TODO: Make abstract type for all metrics(e.g. Gauge, Histogram, Counter) with generic traits so that all metrics can be set up with one function `set`
 #[macro_export]
@@ -99,5 +88,3 @@ macro_rules! prometheus(
   }
 );
 */
-=======
->>>>>>> 5842caed0503c4f4b88bd72485d289a1731a9729
