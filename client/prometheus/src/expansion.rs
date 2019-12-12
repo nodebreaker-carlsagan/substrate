81b8aa5d89ddab77515b12e48a478f17b9ba06b0
nodebreaker-carlsagan <zeroday26@gmail.com>
2019-12-13 04:10:35 +0900

    expansion edit

--- client/prometheus/src/expansion.rs
+++ client/prometheus/src/expansion.rs
@@ -3,8 +3,13 @@ pub use crate::*;
 use sp_runtime::traits::{NumberFor, Block as BlockT};
 use fg_primitives::{AuthorityId,AuthoritySignature};
 //sp_finality_grandpa::app::Signature, sp_finality_grandpa::app::Public is to_raw_vec()
-use sp_core::{ crypto::{ Public }};
-
+use sp_core::{ 
+    crypto::{ 
+        Public,
+        
+    }
+};
+use hex::{encode};
 
 type Blockcast<Block> = grandpa::CatchUp<
 	<Block as BlockT>::Hash,
@@ -24,8 +29,9 @@ pub fn full_message_metrics<Block: BlockT>(
     authorities: Vec<AuthorityId>
 )
 {
-    let _block_num = &message.base_number.clone();
-    let _precommets =  &message.precommits[0].id.clone().to_raw_vec();
+    let mut _block_num = &message.base_number.clone().saturated_into().to_string();
+    let mut _precommets =  &encode(&message.precommits[0].id.clone().to_raw_vec()).to_string();
+    //let string_precommets = Public::from_string(&_precommets);
     //alloc::vec::Vec<finality_grandpa::SignedPrecommit<primitive_types::H256, u32, sp_finality_grandpa::app::Signature, sp_finality_grandpa::app::Public>>
     let _prevotes = &message.prevotes[0].id.clone().to_raw_vec(); 
     //alloc::vec::Vec<finality_grandpa::SignedPrevote<primitive_types::H256, u32, sp_finality_grandpa::app::Signature, sp_finality_grandpa::app::Public>>
@@ -35,6 +41,10 @@ pub fn full_message_metrics<Block: BlockT>(
     println!("{:?}",_precommets);
     println!("{:?}",_prevotes);
     println!("{:?}",_authorityid);
+    let mut labels2 = std::collections::HashMap::new();
+    labels2.insert("validator_address", _precommets as &str);
+    labels2.insert("block_num", _block_num as &str);
+    metrics::set_vecgauge(&metrics::VALIDATOR_SIGN ,&labels2,0);
 }
     //with fn
     //expansion::full_message_metrics(self.current_authorities.clone());
