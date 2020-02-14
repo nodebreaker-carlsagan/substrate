b64ea8f4a8ba586d5e4cddbf135c54acf0593701
nodebreaker-carlsagan <zeroday26@gmail.com>
2020-02-14 13:39:45 +0900

    remove prometheus in Tracing

--- client/cli/src/params.rs
+++ client/cli/src/params.rs
@@ -331,7 +331,6 @@ arg_enum! {
 	pub enum TracingReceiver {
 		Log,
 		Telemetry,
-		Prometheus,
 	}
 }
 
@@ -340,7 +339,6 @@ impl Into<sc_tracing::TracingReceiver> for TracingReceiver {
 		match self {
 			TracingReceiver::Log => sc_tracing::TracingReceiver::Log,
 			TracingReceiver::Telemetry => sc_tracing::TracingReceiver::Telemetry,
-			TracingReceiver::Prometheus => sc_tracing::TracingReceiver::Prometheus,
 		}
 	}
 }
